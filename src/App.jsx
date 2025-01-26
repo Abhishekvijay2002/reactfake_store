c4d2165169aa6108bb12b94dfef60a662d578767
abhishek vijayan <bhshkvijayan@gmail.com>
2025-01-26 23:29:12 +0530

    fake store

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,28 @@ import Footer from './assets/footer';
 import Header from './assets/header';
 import axios from 'axios';
 import Productcard from './assets/productcard';
+import ReactLoading from 'react-loading';
 
 function App() {
   const[products ,setProducts] =useState([]);
+  const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
       axios.get('https://fakestoreapi.com/products').then((res)=> {
-        console.log(res.data);
         setProducts(res.data);
+        setIsLoading(false);
+
       }).catch((err) => {
-        console.log(err);
+       <p>{err}</p>
+       setIsLoading(false);
       })
     },[])
   return (
-    <>
+    <>    
     <Header/>
     <div className='container'>
+    
+      {isLoading ? ( <div className='d-flex justify-content-center align-items-center' style={{ height: 'calc(100vh - 245px)' }}><ReactLoading type={'spin'} color={`fff`} height={100} width={100} /></div>) :(
+      
       <div  className='row g-4 my-1'>
         {products.map((product) => (
           <div key={product.id} className='col-lg-3 col-md-4 col-sm-6'> 
@@ -28,9 +35,11 @@ function App() {
         )
         }
         
-      </div>
-      </div>
-    <Footer/>
+      </div>) }
+
+     
+     </div>
+     <Footer/>
     </>
   )
 }
